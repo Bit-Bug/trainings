642f701ce8c41e23990fa7f4e58a539e2e9b4946
Rim Masmoudi <bitbug.devlab@gmail.com>
2022-07-27 16:04:06 +0100

    stack builder

--- bin/trainings.ts
+++ bin/trainings.ts
@@ -2,20 +2,51 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { TrainingsStack } from '../lib/trainings-stack';
+import * as gitBranch from 'git-branch';
+import { CDKContext } from '../types'
 
-const app = new cdk.App();
-new TrainingsStack(app, 'TrainingsStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+// const app = new cdk.App();
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const createStack = async () => {
+  try {
+    const app = new cdk.App();
+    const context = await getContext(app);
+    const tags: any = {
+      Environment: context.environment,
+    };
+    const stackProps: cdk.StackProps = {
+      env: {
+        region: context.region,
+        account: context.accountNumber
+      },
+      stackName: `${context.appName}-stack-${context.environment}`,
+      description: 'This is a description',
+      tags
+    }
+    new TrainingsStack(app, `${context.appName}-stack-${context.environment}`, context, stackProps);
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  } catch (error) {
+    console.error(error);
+  }
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+
+}
+export const getContext = async (app: cdk.App): Promise<CDKContext> => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const currentBranch = await gitBranch();
+      console.log(`Current git branch: ${currentBranch}`);
+      const environment = app.node.tryGetContext('environments').find((e: any) => e.branchName === currentBranch);
+      console.log(`Current env: ${JSON.stringify(environment, null, 2)}`);
+      const globals = app.node.tryGetContext('globals');
+      console.log(`globals: ${JSON.stringify(globals, null, 2)}`);
+      return resolve({ ...globals, ...environment });
+
+    } catch (error) {
+      console.error(error);
+      return reject();
+    }
+  })
+
+}
+createStack()
--- lib/trainings-stack.ts
+++ lib/trainings-stack.ts
@@ -1,16 +1,37 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import { CDKContext } from "../types";
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class TrainingsStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, context: CDKContext, props?: StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TrainingsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    //s3 bucket
+    const bucket = new s3.Bucket(this, 'demoBucket', {
+      bucketName: `${context.appName}-${context.environment}`,
+      encryption: context.s3Encrypt ? s3.BucketEncryption.S3_MANAGED : s3.BucketEncryption.UNENCRYPTED,
+    });
+    //dynamo table
+    const table = new dynamodb.Table(this, 'demoTable', {
+      tableName: `${context.appName}-${context.environment}`,
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      pointInTimeRecovery: context.ddbPITRecovery,
+      sortKey: { name: 'rangeKey', type: dynamodb.AttributeType.STRING},
+      partitionKey: { name: 'hashKey', type: dynamodb.AttributeType.STRING },
+    });
+    //stack outputs
+    new CfnOutput(this, 'demoBucketArn', {
+      value: bucket.bucketArn,
+      exportName: `${context.appName}-demoBucketArn-${context.environment}`
+    });
+    new CfnOutput(this, 'demoTableArn',{
+      value: table.tableArn,
+      exportName: `${context.appName}-demoTableArn-${context.environment}`
+    })
   }
 }
